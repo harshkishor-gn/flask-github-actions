507dc0447123cf6c64d43c433959dee3ea00cf93
harshkishor-gn <141920156+harshkishor-gn@users.noreply.github.com>
2023-09-04 17:42:06 +0530

    login (#5)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 # Create a Flask web app
 app = Flask(__name__)
@@ -6,7 +6,8 @@ app = Flask(__name__)
 # Define a route for the homepage
 @app.route('/')
 def home():
-    return '<h1>Hello, system! This is My CICD Pipeline test #8</h1>'
+    # return '<h1>Hello, system! This is My CICD Pipeline test #8</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
